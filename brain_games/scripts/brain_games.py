26c3e447e32930d405c449fbd1b22217dbcf75d4
Knstantin-P <k.pavlov.reg@yandex.ru>
2022-12-11 15:09:54 +0300

    fix brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
-from brain_games.cli import welcome_user
+import prompt
+
+
+def welcome_user():
+    print("Welcome to the Brain Games!")
+    user_name = prompt.string('May I have your name? ')
+    print(f'Hello, {user_name}!')
 
 
 def main():
