c0d0f619b1de7362c5832a1f659368469d385e44
Knstantin-P <k.pavlov.reg@yandex.ru>
2022-12-16 18:07:27 +0300

    fix linter error

--- brain_games/game.py
+++ brain_games/game.py
@@ -31,7 +31,8 @@ def print_result(is_user_answer_correct, user_name, user_answer, task_answer):
     if is_user_answer_correct:
         print('Correct!')
     else:
-        print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{task_answer}'.\nLet's try again, {user_name}!")
+        print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{task_answer}'.)
+        print(Let's try again, {user_name}!")
 
 
 def print_game_over(user_name):
