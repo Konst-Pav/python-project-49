25d0322f835b5b16f1821ae23a9568a1b7fca6ae
Knstantin-P <k.pavlov.reg@yandex.ru>
2022-12-16 16:44:06 +0300

    update game.py

---
+++ brain_games/game.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import prompt
+
+
+def play_brain_game(gen_task, user_name, num_of_rounds):
+    i = num_of_rounds
+    while i > 0:
+        task_answer = gen_task()
+        #print(f'!!! Answer {task_answer}')
+        user_answer = get_answer()
+        result = check_answer(user_answer, task_answer)
+        print_result(result, user_name, user_answer, task_answer)
+        if result == False:
+            break
+        i -= 1
+        task_answer = 0
+        print(f'i = {i}')
+    else:
+        print_game_over(user_name)
+
+
+def get_answer():
+    user_answer = prompt.string('Your answer: ')
+    return user_answer
+
+
+def check_answer(user_answer, task_answer):
+    return True if user_answer == str(task_answer) else False
+
+
+def print_result(is_user_answer_correct, user_name, user_answer, task_answer):
+    if is_user_answer_correct:
+        print('Correct!')
+    else:
+        print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{task_answer}'.\nLet's try again, {user_name}!")
+
+
+def print_game_over(user_name):
+    print(f'Congratulations, {user_name}!')
