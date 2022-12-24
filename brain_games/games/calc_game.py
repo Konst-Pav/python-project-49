bb2d33d77a13fc4dd42e60bf39be4622628ee2a3
Knstantin-P <k.pavlov.reg@yandex.ru>
2022-12-24 18:54:31 +0300

    fix linter errors

--- brain_games/games/calc_game.py
+++ brain_games/games/calc_game.py
@@ -11,9 +11,9 @@ GAME_RULE = 'What is the result of the expression?'
 def generate_calc_question():
     random_num_1 = randint(MIN_NUM, MAX_NUM)
     random_num_2 = randint(MIN_NUM, MAX_NUM)
-    math_operation = choice(LIST_OF_OPERATORS)
-    question = str(random_num_1) + ' ' + str(math_operation) + ' ' + str(random_num_2)
-    answer = calculate_expression(random_num_1, random_num_2, math_operation)
+    math_oper = choice(LIST_OF_OPERATORS)
+    question = str(random_num_1) + ' ' + str(math_oper) + ' ' + str(random_num_2)
+    answer = calculate_expression(random_num_1, random_num_2, math_oper)
     question_and_answer = (question, answer)
     return question_and_answer
 
