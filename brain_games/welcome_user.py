1247f0ef110b62bde740fa71c1719fdc27e5e109
Knstantin-P <k.pavlov.reg@yandex.ru>
2022-12-24 14:33:04 +0300

    update cli.py

--- brain_games/welcome_user.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import prompt
-
-
-def get_user_name():
-    print("Welcome to the Brain Games!")
-    user_name = prompt.string('May I have your name? ')
-    print(f'Hello, {user_name}!')
-    return user_name
